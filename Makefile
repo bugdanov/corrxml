506e6309ff3c11935fd2d18f2824751ac342032f
Rurik Bogdanov <rurik.bugdanov@alsenet.com>
2017-02-06 21:01:53 +0100

    back to initial functionality

---
+++ Makefile
@@ -0,0 +1,5 @@
+PREFIX?=/usr/local
+install:
+	install -D corrxml.sh ${PREFIX}/bin/
+	install -D progressive/progressive ${PREFIX}/bin/
+
--- corrxml.sh
+++ corrxml.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-#  corrxml.sh
-#
 #
 # corrxml - ImageJ-Elphel configuration file modifier
 #
 # Copyright (c) 2014 FOXEL SA - http://foxel.ch
-# Please read <http://foxel.ch/license> for more information.
+# Copyright (c) 2016 ALSENET SA - http://alsenet.com
 #
 #
 # Author(s):
 #
-#      Luc Deschenaux <l.deschenaux@foxel.ch>
+#      Luc Deschenaux <luc.deschenaux@freesurf.ch>
 #
 #
 # This file is part of the FOXEL project <http://foxel.ch>.
+# Please read <http://foxel.ch/license> for more information.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -40,125 +39,97 @@
 
 [ -n "$DEBUG" ] && set -x
 
-if [ "$1" = "-s" -o "$1" = "--shuffle" ] ; then
-  SHUFFLE=1
-  shift
-fi
+usage() {
+cat << EOF
+NAME $(basename $0) - Generate xml file for imagej-elphel Eyesis_Correction
 
-if [ $# -lt 2 -o $# -gt 7 ] ; then
-  echo "usage: $(basename $0) [ -s  | --shuffle ] <base_config> <path_to_jp4_files> [ <results_directory> <outfile> <split_at> <timestamp> <truncate> ]"
-  echo
-  echo "When <truncate> is specified after <timestamp>, <split_at> does skip remaining"
-  echo "jp4s with the same timestamp (eg: when you want only the 8 first channels per"
-  echo "timestamp). Otherwise a trailing sequence number is added to <outfile> for"
-  echo "the remaining xml(s), like when <timestamp> is not specified."
-  echo
+SYNOPSIS
+    $(basename $0) -b <base_xml_config> [-f <jp4_file_list>] [-o <output_file>]
+
+    -b|--base_config <base_xml_config>    The imagej-elphel base xml config
+
+    -o|--output <output_file>             The resulting xml file
+
+    -f|--filelist <jp4_file_list>         The list of jp4 files to process,
+                                          if not specified, read from stdin
+
+    -d|--define <variable=name>           CORRECTION_PARAMETERS to change
+                                          Must be one of:
+                                            - resultsDirectory
+                                            - tiffCompression
+                                            - smoothKernelDirectory
+                                            - sharpKernelDirectory
+                                            - sensorDirectory
+                                            - equirectangularDirectory
+DESCRIPTION
+    The base config is stripped from existinig source paths.
+
+    The defined CORRECTION_PARAMETERS are set.
+
+    They can also be specified as environment variables.
+    eg: resultsDirectory=\$HOME/here $(basename $0) ...
+
+    Files in the given file list are added as sourcePaths in the output xml.
+
+EOF
   exit 1
-fi
-
-BASE_CONFIG="$1"
-DIR="$2"
-[ -n "$3" ] && export RESULTSDIRECTORY=$3
-OUTFILE_EXT=$(basename "$4" | sed -r -e 's/.*\.([^\.]+)$/\1/')
-OUTFILE=$(echo "$4" | sed -r -e 's/\.[^\.]+$//')
-SPLIT_AT=$(($5+0))
-TIMESTAMP=$6
-TRUNCATE=$7
-CHUNK_NUM=0
-TMP_CONFIG="/tmp/config.$$.tmp"
-TOSTDOUT=0
-NOW=$(date +%s) # _%N
-
-if [ -z "$OUTFILE" ] ; then
-  TOSTDOUT=1
-  OUTFILE=/tmp/tmp.$$
-  # cannot split stdout
-  [ -z "$TIMESTAMP" ] && SPLIT_AT=0
-fi
-
-footer() {
-    echo "<entry key=\"CORRECTION_PARAMETERS.sourcePaths\">$1</entry>"
-    echo '</properties>'
 }
 
-egrep -v -e 'CORRECTION_PARAMETERS\.sourcePaths' -e 'CORRECTION_PARAMETERS.sourcePath[0-9]+' -e '</properties>' "$BASE_CONFIG" > "$TMP_CONFIG"
+  # parse command line options
+  if ! options=$(getopt -o hb:i:o:d: -l help,base-config:,input:,output:,define: -- "$@")
+  then
+      # something went wrong, getopt will put out an error message for us
+      exit 1
+  fi
+ 
+  eval set -- "$options"
 
-for property in smoothKernelDirectory sharpKernelDirectory sensorDirectory equirectangularDirectory resultsDirectory ; do
-  dir=$(env|grep -i $property | cut -f 2 -d '=')
-  if [ -n "$dir" ] ; then
-     sed -r -i -e 's#CORRECTION_PARAMETERS\.'$property'.*#CORRECTION_PARAMETERS.'$property'">'$dir'</entry>#' "$TMP_CONFIG"
+  FILELIST=/dev/stdin 
+
+  while [ $# -gt 0 ] ; do
+      case $1 in
+      -h|--help) usage ;;
+      -b|--base-config) BASE_CONFIG="$2" ; shift ;;
+      -f|--filelist) FILELIST="$2" ; shift ;;
+      -d|--define) eval "export $2" || usage ; shift ;;
+      -o|--output) OUTFILE="$2" ; shift  ;;
+      (--) shift; break;;
+      (-*) echo "$(basename $0): error - unrecognized option $1" 1>&2; exit 1;;
+      (*) break;;
+      esac
+      shift
+  done
+ 
+
+[ -f "$BASE_CONFIG" ] || usage
+TEMPFILE=$(mktemp)
+
+
+# remove sourcePaths and ending tag from base config
+egrep -v -e 'CORRECTION_PARAMETERS\.sourcePaths' -e 'CORRECTION_PARAMETERS.sourcePath[0-9]+' -e '</properties>' "$BASE_CONFIG" > $TEMPFILE
+
+# set parameters specified as environment variables
+for property in smoothKernelDirectory sharpKernelDirectory sensorDirectory equirectangularDirectory resultsDirectory tiffCompression; do
+  VALUE=$(env|grep -i $property | cut -f 2 -d '=')
+  if [ -n "$VALUE" ] ; then
+     sed -r -i -e 's#CORRECTION_PARAMETERS\.'$property'.*#CORRECTION_PARAMETERS.'$property'">'$VALUE'</entry>#' $TEMPFILE || echo "<entry key=\"CORRECTION_PARAMETERS.$property\">$VALUE</entry>" >> $TEMPFILE
   fi
 done
 
-cat "$TMP_CONFIG" > "${OUTFILE}.tmp"
+# add files from list
+INDEX=0
+while read JP4 ; do
+  ((++INDEX))
+  echo "<entry key=\"CORRECTION_PARAMETERS.sourcePath${INDEX}\">$JP4</entry>" >> $TEMPFILE
+done < $FILELIST
 
-NUM=0
-COUNTFILE=/tmp/count.$$.tmp
-echo 0 > $COUNTFILE
+echo "<entry key=\"CORRECTION_PARAMETERS.sourcePaths\">$INDEX</entry>" >> $TEMPFILE
+echo '</properties>' $TEMPFILE
 
-getTimestamps() {
-  sed -r -n -e 's/.*([0-9]{10}_[0-9]{6}).*/\1/p'
-}
-
-getfilelist() {
-  if [ -z "$FILELIST" ]; then
-    FILELIST="/tmp/filelist_$NOW.tmp"
-    find "$DIR" -name $TIMESTAMP\*.jp4 | sort > $FILELIST
-  fi
-  if [ -n "$TIMESTAMP" ] ; then
-    grep -E -e "${TIMESTAMP}_[0-9]+.jp4" "$FILELIST"
-  else
-    if [ -n "$SHUFFLE" ] ; then
-      cat "$FILELIST" | getTimestamps | sort -u | progressive | while read ts ; do
-        grep $ts "$FILELIST"
-      done
-    else
-      cat "$FILELIST"
-    fi
-  fi
-}
-
-getfilelist | while read JP4 ; do
-  echo "<entry key=\"CORRECTION_PARAMETERS.sourcePath${NUM}\">$JP4</entry>" >> "${OUTFILE}.tmp"
-  NUM=$(($NUM+1)) 
-  if [ $SPLIT_AT -eq $NUM ] ; then
-    footer $NUM >> "${OUTFILE}.tmp"
-    if [ -n "$TIMESTAMP" -a -n "$TRUNCATE" ] ; then
-        mv "${OUTFILE}.tmp" "${OUTFILE}.$OUTFILE_EXT"
-        echo "${OUTFILE}.$OUTFILE_EXT"
-        echo 0 > $COUNTFILE
-        break
-    fi
-    CHUNK_NUM=$(($CHUNK_NUM+1))
-    mv "${OUTFILE}.tmp" "${OUTFILE}.$(printf "%05d" $CHUNK_NUM).$OUTFILE_EXT"
-    echo "${OUTFILE}.$(printf "%05d" $CHUNK_NUM).$OUTFILE_EXT"
-    cat "$TMP_CONFIG" > "${OUTFILE}.tmp"
-    NUM=0
-  fi
-  echo $NUM > $COUNTFILE
-done
-
-NUM=$(cat $COUNTFILE)
-rm $COUNTFILE
-
-if [ $NUM -eq 0 ] ; then 
-  [ -f "${OUTFILE}.tmp" ] && rm "${OUTFILE}.tmp"
-  if [ $TOSTDOUT -eq 1 ] ; then
-    [ -s "$OUTFILE" ] && cat $OUTFILE
-    rm $OUTFILE
-  fi
-  exit 0
+if [ -n "$OUTFILE" ] ; then 
+  mv $TEMPFILE "$OUTFILE"
+else
+  cat $TEMPFILE
 fi
 
-footer $NUM  >> "${OUTFILE}.tmp"
-
-if [ $TOSTDOUT -eq 1 ] ; then
-  cat "${OUTFILE}.tmp"
-  rm "${OUTFILE}.tmp"
-  exit 0
-fi
-
-[ $CHUNK_NUM -gt 1 ] && SUFFIX=.$((CHUNK_NUM-1))
-mv "${OUTFILE}.tmp" "${OUTFILE}$SUFFIX.$OUTFILE_EXT"
-echo "${OUTFILE}$SUFFIX.$OUTFILE_EXT"
 
